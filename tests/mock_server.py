20bb46cddcbb6ffb5f446ef13a04e760ec921811
Jonáš Nedvědický <Jonas.Ned@outlook.com>
2026-09-18 07:48:07 +0200

    test: add user info endpoint to API mock

--- tests/mock_server.py
+++ tests/mock_server.py
@@ -54,6 +54,21 @@ MARKS = {
     ]
 }
 
+USER_INFO = {
+    "UserUID": "mock-user",
+    "FullName": "Test Student",
+    "FirstName": "Test",
+    "LastName": "Student",
+    "Class": {
+        "Id": "01",
+        "Abbrev": "8.A",
+        "Name": "8.A",
+        "Teacher": {"Name": "Jan Test"}
+    },
+    "UserType": "student",
+    "UserTypeText": "žák"
+}
+
 ABSENCE = {
     "PercentageThreshold": 0.18,
     "Absences": [
@@ -94,7 +109,7 @@ class Handler(BaseHTTPRequestHandler):
             self._json(401, {"error": "unauthorized"})
             return
 
-        if self.path == "/api/3/timetable/actual":
+        if self.path == "/api/3/user":\n            self._json(200, USER_INFO)\n        elif self.path == "/api/3/timetable/actual":
             self._json(200, TIMETABLE)
         elif self.path == "/api/3/homeworks":
             self._json(200, HOMEWORKS)
